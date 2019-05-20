a0db2cefd4ee1a994a2cea723d2b8fd375beb925
Antony <A.Krutenok@gmail.com>
2019-05-20 11:59:16 +0300

    Updates

---
+++ precache-manifest.6f621e77b00633467441913df8c4097e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26bf890c2ca7a69f0bc5cc9a11627f8",
+    "url": "/ttt/index.html"
+  },
+  {
+    "revision": "619ffcd45e28567a89a7",
+    "url": "/ttt/static/css/main.fd52d94e.chunk.css"
+  },
+  {
+    "revision": "8d3b7f166cb01218cef1",
+    "url": "/ttt/static/js/2.22579f8f.chunk.js"
+  },
+  {
+    "revision": "619ffcd45e28567a89a7",
+    "url": "/ttt/static/js/main.adda18ea.chunk.js"
+  },
+  {
+    "revision": "dbdc744f9f9995a9224f",
+    "url": "/ttt/static/js/runtime~main.b9542834.js"
+  }
+]);
