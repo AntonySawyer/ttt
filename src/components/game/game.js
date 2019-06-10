0e8f3526046378d1a17469cbe7b82c800be32f03
Antony <A.Krutenok@gmail.com>
2019-06-10 17:43:56 +0300

    feat: add stupid(aka random) ai

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -1,6 +1,9 @@
 import React from 'react';
+
 import Board from '../board/board';
 import GameControls from '../gameControls/gameControls';
+import Status from '../status/status';
+import { aiEasy } from '../../utils/aiEasy';
 import checkWinner from '../../utils/checkWinner';
 import './game.css';
 
@@ -10,7 +13,9 @@ export default class Game extends React.Component {
         this.state = {
             squares: Array(9).fill(),
             lastMove: [],
-            turn: 'X'
+            turn: 'X',
+            aiMove: false,
+            aiMark: 'O'
         };
     }
 
@@ -20,24 +25,37 @@ export default class Game extends React.Component {
             squares[i] = this.state.turn;
             const lastMove = [...this.state.lastMove];
             lastMove.push(i);
+            if (this.state.aiMove && this.state.aiMark !== this.state.turn) {
+                const aiMove = aiEasy(squares);
+                squares[aiMove] = this.state.aiMark;
+                lastMove.push(aiMove);
+            } else {
+                this.changeTurn();
+            }
             this.setState({squares: squares, 
-                            lastMove: lastMove
-                        });
-            this.changeTurn();
-            console.log(squares.filter(i => i).length % 2 === 1);
+                lastMove: lastMove
+            });
         }
+    }
+
+    initAI() {
+        this.setState({aiMove: document.querySelector('#easyComp').checked});
     }
 
     changeTurn() {
-        this.setState({turn: (this.state.turn === 'X' && 'O') || 'X'});
+        const newTurn = (this.state.turn === 'X' ? 'O' : 'X');
+        this.setState({turn: newTurn});
     }
 
     undoMove() {
-        if (this.state.lastMove.length !== 0) {
+        const newSquares = [...this.state.squares];
+        if (this.state.aiMove) {
+            newSquares[this.state.lastMove.pop()] = null;
+            newSquares[this.state.lastMove.pop()] = null;
+        } else {
+            newSquares[this.state.lastMove.pop()] = null;
             this.changeTurn();
         }
-        const newSquares = [...this.state.squares];
-        newSquares[this.state.lastMove.pop()] = null;
         this.setState({squares: newSquares});
         this.unsetClasses();
     }
@@ -56,17 +74,20 @@ export default class Game extends React.Component {
 
     render() {
         const winner = checkWinner(this.state.squares);
-        const status = (winner && <p>Result of game - <span className="gameOver">{winner}</span></p>) 
-                        || <p>Next move - {this.state.turn}</p>;
+        const isGameStarted = this.state.lastMove.length !== 0;
 
       return (
         <div className="game">
-            <div className="gameStatus">{status}</div>
+            <Status gameOver={winner} turn={this.state.turn} />
             <Board squares={this.state.squares} onClick={(i) => this.makeMove(i)} />
-            <GameControls disabledControls={this.state.lastMove.length !== 0} 
+            <GameControls disabledControls={isGameStarted} 
                             changeOnClick={() => this.changeTurn()} 
                             undoOnClick={() => this.undoMove()} 
                             clearOnClick={() => this.clearField()} />
+            <label>
+                <input type="checkbox" id="easyComp" disabled={isGameStarted} onChange={() => this.initAI()} />
+            easy AI
+            </label>
         </div>
       );
     }
